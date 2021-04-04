2d5690b9120335a9bbafb2ae0b565e60500bd973
Joakim Sandström <joakim.s@webbkontakt.net>
2021-04-04 14:17:58 +0200

    Initial script for 18.x
    Thanks to fzinken on discourse.osmc.tv, for the ground work. Just took his post and made it into an addon. fzinken changed the password to Test2 inhis post, but a thought a "restore" to factory default would be the obvious choice for a addon. Might develop this to " chose" password in the future.

---
+++ addon.py
@@ -0,0 +1,27 @@
+"""
+    Plugin for to factory restore osmc user password, thanks fzinken for the ground work
+"""
+
+# -*- coding: UTF-8 -*-
+# main imports
+import sys
+import os
+import xbmc
+import xbmcgui
+import xbmcaddon
+import time
+
+# plugin constants
+__plugin__ = "Factory restore osmc user password "
+__author__ = "Zjoasan"
+__url__ = "https://osmc.tv/"
+__git_url__ = "https://https://github.com/zjoasan/osmc_passwd_restore"
+__credits__ = "Zjoasan"
+__version__ = "0.0.1"
+
+dialog = xbmcgui.Dialog()
+addon = xbmcaddon.Addon(id='plugin.program.OSMC_pw_restore')
+
+os.system("/bin/echo -e 'osmc\nosmc' | /usr/bin/sudo /usr/bin/passwd osmc")
+time.sleep(5)
+dialog.ok("Password restored", "Now the password for osmc user has been restored to factory default.")
