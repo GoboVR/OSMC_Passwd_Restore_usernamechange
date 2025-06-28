729beb98ae85e09ddd4a88c0c7d6563c8d8c0570
GoboVR <113143929+GoboVR@users.noreply.github.com>
2025-06-28 14:56:50 -0400

    Update addon.py

--- addon.py
+++ addon.py
@@ -1,5 +1,5 @@
 """
-    Plugin for to factory restore osmc user password
+    Plugin for to factory restore gobo user password
 """
 
 # -*- coding: UTF-8 -*-
@@ -12,16 +12,16 @@ import xbmcaddon
 import time
 
 # plugin constants
-__plugin__ = "Factory restore osmc user password "
-__author__ = "Zjoasan"
+__plugin__ = "Factory restore gobo user password "
+__author__ = "Orignail: Zjoasan Fork: Gobo and Gemini AI"
 __url__ = "https://osmc.tv/"
-__git_url__ = "https://https://github.com/zjoasan/osmc_passwd_restore"
-__credits__ = "Zjoasan"
+__git_url__ = "https://github.com/GoboVR/OSMC_Passwd_Restore_usernamechange"
+__credits__ = "Orignail: Zjoasan Fork: Gobo and Gemini AI"
 __version__ = "0.0.1"
 
 dialog = xbmcgui.Dialog()
-addon = xbmcaddon.Addon(id='plugin.program.OSMC_pw_restore')
+addon = xbmcaddon.Addon(id='plugin.program.Gobo_pw_restore')
 
-os.system("/bin/echo -e 'osmc\nosmc' | /usr/bin/sudo /usr/bin/passwd osmc")
+os.system("/bin/echo -e 'gobo\ngobo' | /usr/bin/sudo /usr/bin/passwd gobo") # Changed line
 time.sleep(5)
-dialog.ok("Password restored", "Now the password for osmc user has been restored to factory default.")
+dialog.ok("Password restored", "Now the password for gobo user has been restored to factory default.") # Changed dialog message
